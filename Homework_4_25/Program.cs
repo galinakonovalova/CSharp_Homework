c039a82e55fcc969c478d7dd0915b21378e98124
Galina Konovalova <galina.konovalova85@gmail.com>
2023-03-19 13:19:45 +0300

    Задача 25

---
+++ Homework_4_25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите число:");
+int b = Convert.ToInt32(Console.ReadLine()); 
+
+int power = 1;
+for(int i=0;i<b;i++)
+{
+  power*=a;
+}
+
+System.Console.WriteLine($"Степень {power}");
