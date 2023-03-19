21e046c089beac170a9bd206169db2b95a5b5369
Galina Konovalova <galina.konovalova85@gmail.com>
2023-03-19 13:20:24 +0300

    Задача 27

---
+++ Homework_4_27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+Console.Write("Введите число:");
+int one = Convert.ToInt32(Console.ReadLine()); 
+
+int result=mneSum(one);
+Console.WriteLine(result);
+
+int mneSum(int x)
+{
+  int sum=0;
+  
+  while(x > 0) //условие выхода
+  {
+    int digit = x % 10;
+    sum += digit;
+
+    x=x/10;//шаг
+  } 
+  return sum; 
+}
+
