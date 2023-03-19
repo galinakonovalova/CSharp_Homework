25c3ff01c5c07bf6d3d5ec946b4163dffc4f82ba
Galina Konovalova <galina.konovalova85@gmail.com>
2023-03-19 13:21:03 +0300

    Задача 29

---
+++ Homework_4_29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+
+fill(array);
+print(array);
+
+void fill(int[] array)
+{
+  Random ran = new Random();
+  for (int i=0; i<array.Length;i++)
+  {
+    array[i] = ran.Next(0, 100);
+  }
+}
+
+void print(int[] array)
+{
+  foreach (int v in array)
+  {
+    System.Console.Write($"{v} ");
+  }
+}
