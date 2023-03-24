2b9986373925b090c3378b514b6d9cb300ac1f97
Galina Konovalova <galina.konovalova85@gmail.com>
2023-03-24 14:24:26 +0300

    Задача 38

---
+++ Homework_5_38/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между
+// максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+double[] array = new double[10];
+
+fillDouble(array);
+printDouble(array);
+
+
+
+void fillDouble(double[] array)
+{
+  Random ran = new Random();
+  for (int i=0; i<array.Length;i++)
+  {
+    array[i] = ran.NextDouble();
+  }
+}
+
+void printDouble(double[] array)
+{
+  foreach (double v in array)
+  {
+    System.Console.Write($"{v} ");
+  }
+}
+
+
