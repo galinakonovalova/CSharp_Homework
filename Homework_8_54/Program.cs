90375e222527d6a2c8fb175906725c5aee206028
Galina Konovalova <galina.konovalova85@gmail.com>
2023-04-05 16:58:41 +0300

    Семинар 8 Задача 54

---
+++ Homework_8_54/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки 
+// двумерного массива.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+  var rnd = new Random();
+  var matrix = new int[rows, columns];
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rnd.Next(min, max + 1);
+    }
+
+  }
+  return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    Console.Write("|");
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j],4} ");
+    }
+    Console.WriteLine("|");
+  }
+}
+
+void ReversSort(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int p = j + 1; p < matrix.GetLength(1); p++)
+            {
+                if (matrix[i, j] < matrix[i, p])
+                {
+                    int t = matrix[i, j];
+                    matrix[i, j] = matrix[i, p];
+                    matrix[i, p] = t;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4, 0, 10);
+PrintMatrix(matrix);
+
+Console.WriteLine();
+
+ReversSort(matrix);
+PrintMatrix(matrix);
