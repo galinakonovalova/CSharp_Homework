0e943da7d7158629c4d977d463631c8de8a8c8a9
Galina Konovalova <galina.konovalova85@gmail.com>
2023-04-17 15:56:34 +0300

    Семинар 9 Задача 68

---
+++ Homework_9_68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.WriteLine($"Введите первое положительное число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Введите второе положительное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = Akkerman(m, n);
+Console.WriteLine($"Функция Аккермана {sum}");
+
+int Akkerman(int n, int m)
+{
+  while (n != 0)
+  {
+    if (m == 0)
+      m = 1; 
+    else
+      m = Akkerman(n, m - 1);
+    n = n - 1;
+  }
+  return m + 1;
+}
+
